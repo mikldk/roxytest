63abd5caa9e6d2ce1ca0661072dea2343d2397ea
rubak <rubak@math.aau.dk>
2020-05-14 01:05:09 +0200

    Improve check of tinytest usage.

--- R/verify_tinytest.R
+++ R/verify_tinytest.R
@@ -1,16 +1,10 @@
 verify_tinytest_used <- function() {
-  if (!dir.exists("inst") || !dir.exists(file.path("inst", "tinytest"))) {
-    stop("tinytest is not currently used, please set it up (create dir inst/tinytest)")
+  if (!dir.exists("tests") || 
+      !file.exists(file.path("tests", "tinytest.R")) ||
+      !dir.exists(file.path("inst", "tinytest"))) {
+    stop("tinytest is not currently used, please set it up, e.g. by tinytest::setup_tinytest()")
     return(FALSE)
   }
-  
-  # if (!file.exists(file.path("tests", "testthat.R"))) {
-  #   return(FALSE)
-  # }
-  # 
-  # if (!dir.exists(file.path("tests", "testthat"))) {
-  #   return(FALSE)
-  # }
-  
+
   return(TRUE)
 }
